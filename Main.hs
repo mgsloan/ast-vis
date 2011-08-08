564a1076c8e409ea1e4decf13c6505f8a12d15de
Michael Sloan <mgsloan@lappy.(none)>
2011-08-08 03:09:56 -0700

    Cleanups / better drawing

--- Main.hs
+++ Main.hs
@@ -1,12 +1,9 @@
-{-# LANGUAGE StandaloneDeriving, DeriveDataTypeable, FlexibleInstances,
-TemplateHaskell, MultiParamTypeClasses, TypeSynonymInstances, TupleSections #-}
+{-# LANGUAGE FlexibleInstances, TupleSections #-}
 
 import Control.Monad (liftM, zipWithM)
-import Control.Arrow ((&&&))
-import Data.Monoid (mconcat)
+import Control.Arrow ((&&&), first)
 
 import Language.Haskell.Exts.Annotated
-import Language.Haskell.Exts.Annotated.Syntax
 import qualified Graphics.Rendering.Cairo as C
 
 import Data.Curve
@@ -14,81 +11,73 @@ import qualified Data.Curve.Interval as I
 
 import Graphics.ToyFramework
 
-import Debug.Trace
-
 import Data.Maybe
 import Data.Either
-import Data.Either.Unwrap
 import Data.List (groupBy)
 import Data.Function (on)
 
 import Data.Data
-import Data.DeriveTH
--- import Data.Generics.PlateTypeable
-import Data.Generics.Uniplate.Data
-import GHC.Real (Ratio(..))
 import Data.Generics.Aliases
-import Text.ParserCombinators.ReadP
-
-{-
-$( derives [makeUniplateTypeable] [''Decl, ''SrcSpan, ''SrcSpanInfo,
-  -- Immediate dependencies of Decl
-  ''Kind, ''FunDep, ''Op, ''Match, ''Rhs, ''CallConv, ''QName, ''Annotation,
-  ''Activation, ''Rule, ''Exp, ''ClassDecl, ''InstDecl, ''Pat, ''Name, ''Context,
-  ''InstHead, ''Type, ''Safety, ''Binds, ''Assoc, ''DeclHead, ''DataOrNew,
-  ''GadtDecl, ''QualConDecl,
-  
-  -- Dependencies of Exp
-  ''Alt, ''QOp, ''XAttr, ''Splice, ''FieldUpdate, ''Literal, ''QualStmt, ''XName,
-  ''Bracket, ''Stmt, ''IPName,
-
-  ''TyVarBind, ''IPBind, ''ConDecl, ''FieldDecl, ''BangType,
-  ''GuardedAlts, ''Boxed, ''Asst, ''PXAttr, ''PatField, ''RPat, ''Deriving,
-  ''RuleVar, ''SpecialCon, ''ModuleName, ''GuardedRhs, ''GuardedAlt, ''RPatOp
-  ] )
--}
-
--- TySynD GHC.Real.Rational [] (AppT (ConT GHC.Real.Ratio) (ConT GHC.Integer.Type.Integer))
-
---instance (Typeable a, Uniplate a) => PlateAll Rational a where
---  plateAll (x :% y) = plate (:%) |+ x |+ y
 
 main = runToy $ Toy
-  { initialState = initial
-  , display = handleDisplay
-  , key     = handleKey
+  { initialState = State "f = a . b . c $ 4 * 2 + 1" 0
+      (ParseFailed (SrcLoc "" 0 0)  "")
+      (0, 220)
   , mouse   = handleMouse
-  , tick    = update
+  , key     = handleKey
+  , display = handleDisplay
+  , tick    = const return
   }
 
-data State = State String Int (ParseResult (Decl SrcSpanInfo))
+handleMouse :: Maybe (Bool, Int) -> DPoint -> State -> IO State
+handleMouse _ d (State a b c _) = return $ State a b c d
 
-initial = State "1 + 1" 0 (ParseFailed (SrcLoc "" 0 0)  "")
+updateParse (State str ix _ m) = State str ix (parseDecl str) m
 
---hline x1 x2 y = ((x1, y), (x2, y))
---vline x y1 y2 = ((x, y1), (x, y2))
+handleKey :: Either [Char] Char -> Bool -> State -> IO State
+handleKey (Right k) True (State x ix p m) =
+  return . updateParse $ State (insIx [k] ix x) (ix + 1) p m
 
-gshows2 :: Data a => Int -> a -> ShowS
+handleKey (Left k) True s@(State x ix p m) = liftM updateParse $ (case k of
+    "Left"  -> modIx (max 0 . subtract 1)
+    "Right" -> modIx (min endPos . (+1))
+    "Home"  -> modIx (const 0)
+    "End"   -> modIx (const endPos)
+    "BackSpace" -> return $ State (delIx (ix - 1) x) (max 0 (ix - 1)) p m
+    "Delete" -> return $ State (delIx ix x) ix p m
+    "Escape" -> error "User escape"
+    _ -> return s)
+  where modIx f = return $ State x (f ix) p m
+        endPos = length x
 
-gshows2 depth = ( \t ->
-                showString ("(" ++ show depth)
-              . (showString . showConstr . toConstr $ t)
-              . (foldr (.) id . gmapQ ((showChar ' ' .) . gshows2 (depth + 1) ) $ t)
-              . showChar ')'
-         ) `extQ` (shows :: String -> ShowS)
-           `extQ` (shows . srcSpan)
-
-single x = [x]
-
-{-
-instance (Monoid a, Monoid b) => Monoid (Either a b) where
-  mempty = Left mempty
-  mappend (Left x) ()
-  mappend (Left
- -}
+handleKey _ _ s = return s
 
 
-debug x = trace (show x) x
+handleDisplay :: IPnt -> IRect -> State -> C.Render State
+handleDisplay _ (tl, br) s@(State xs ix p m) = do
+  C.moveTo 10 200
+  C.showText xs
+  (textRect (10.0, 200.0) xs 0 ix) >>= draw . rside 1
+  C.stroke
+  
+  C.moveTo 10 150
+  case p of
+    ParseOk x -> (toSegs ((10.0, snd m - fromIntegral height * 0.5))
+               . process . rights . catMaybes $ glines 0 x)
+             >>= mapM_ draw
+    f@(ParseFailed _ _) -> C.showText (show f)
+  C.stroke
+
+  return s
+ where
+  process = map last . groupBy ((==) `on` (\(x,_,_)->x))
+  toSegs p = zipWithM (\d ((f, t), _, txt) ->
+               liftM ((txt,) . rside 0 . first (I.expand 2)) $
+               textRect (p ^+^ ((0,15) ^* (fromIntegral d))) xs (f-1) (t-1)) [1..]
+  height = snd $ br ^-^ tl
+data State = State String Int (ParseResult (Decl SrcSpanInfo)) (Double, Double)
+
+srcSpan = (srcSpanStartColumn &&& srcSpanEndColumn) . srcInfoSpan
 
 glines :: (Data a) => Int -> a -> [Maybe (Either (Int, Int) ((Int, Int), Int, String))]
 glines depth = (\t -> (\xs -> helper (show $ toConstr t) xs : xs)
@@ -97,62 +86,17 @@ glines depth = (\t -> (\xs -> helper (show $ toConstr t) xs : xs)
   where helper name xs = (listToMaybe . lefts $ catMaybes xs) >>=
                          return . Right . (, depth, name)
 
-
-srcSpan = (srcSpanStartColumn &&& srcSpanEndColumn) . srcInfoSpan
+-- Util
+single x = [x]
+insIx v i xs = pre ++ v ++ post
+  where (pre, post) = splitAt i xs
+delIx i xs | (pre, (_:post)) <- splitAt i xs = pre ++ post
+           | otherwise = xs
 
 instance Draw (String, DLine) where
   draw (txt, x) = draw x >> move (x `at` 0.5) >> C.showText txt
 
-handleDisplay :: IPnt -> IRect -> State -> C.Render State
-handleDisplay _ _ s@(State xs ix p) = do
-  C.moveTo 10 200
-  C.showText xs
-  (textRect (10.0, 200.0) xs 0 ix) >>= draw . rside 1
-  C.stroke
-  
-  C.moveTo 10 150
-  case p of
-    ParseOk x -> (toSegs (0.0, 220.0) . process . rights . catMaybes $ glines 0 x)
-             >>= mapM_ draw
-    f@(ParseFailed _ _) -> C.showText (show f)
-  C.stroke
-
-  return s
- where
-  process = map last . groupBy ((==) `on` (\(x,_,_)->x))
-  toSegs p = zipWithM (\d ((f, t), _, txt) -> liftM ((txt,) . rside 0) $
-               textRect (p ^+^ ((0,15) ^* (fromIntegral d))) xs f t) [1..]
-
-handleKey :: Either [Char] Char -> Bool -> State -> IO State
-handleKey (Right k) True = return . updateParse .
-                           (\(State x ix p) -> State (insIx [k] ix x) (ix + 1) p)
-handleKey (Left k) True = liftM updateParse . ((case k of
-    "Left" -> modIx (max 0 . subtract 1)
-    "Right" -> modIx (1+)
-    "BackSpace" -> return . (\(State x ix p) -> State (delIx (ix - 1) x) (max 0 (ix - 1)) p)
-    "Delete" -> return . (\(State x ix p) -> State (delIx ix x) ix p)
-    "Escape" -> error "User escape"
-    _ -> return) :: State -> IO State)
-  where modIx :: (Int -> Int) -> State -> IO State
-        modIx f (State x ix p) = return $ State x (f ix) p
-handleKey _ _ = return
-
-updateParse (State str ix _) = State str ix (parseDecl str)
-
-handleMouse :: Maybe (Bool, Int) -> DPoint -> State -> IO State
-handleMouse _ _ = return
-
-update _ s = return s
-
--- Util
-insIx v i xs = pre ++ v ++ post     where (pre, post)     = splitAt i xs
-delIx i xs | (pre, (x:post)) <- splitAt i xs = pre ++ post
-           | otherwise = xs
-
 textRect :: DPoint -> String -> Int -> Int -> C.Render DRect
-textRect (x, y) txt 0 t = do
-  (C.TextExtents _ _ _ _ w h) <- C.textExtents (take t txt)
-  return $ (x + 2 I.... x + w, y - 10 I.... y + 2)
 textRect (x, y) txt f t | t >= f = do
   (C.TextExtents _ _ _ _ w1 _) <- C.textExtents pre
   (C.TextExtents _ _ _ _ w2 _) <- C.textExtents (take (t - f) post)
